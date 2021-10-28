c1751385c33625d22ff842c9c1310082ac840f38
sikeda <sikeda@FMX-P20210403.local>
2021-10-28 09:22:17 +0900

    トップページ、ロゴの作成

---
+++ src/layouts/DefaultLayout/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="default_layout">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Top from "@/pages/Top/Top";
+import LaunchLayout from "@/layouts/LaunchLayout/LaunchLayout";
+
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: LaunchLayout,
+    children: [
+      {
+        path: '',
+        name: 'Top',
+        component: Top,
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = createRouter({
