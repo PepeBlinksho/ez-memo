85978efb3a2ff2436b600ed8f471716d4961b8b9
sikeda <sikeda@FMX-P20210403.local>
2021-11-04 16:00:43 +0900

    ボタンの作成

--- src/pages/Elements.vue
+++ src/pages/Elements.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="elements">
-    <div class="container-fuld">
+    <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <button type="button" class="btn btn-primary m-4">Primary</button>
-          <button type="button" class="btn btn-secondary m-4">Secondary</button>
-          <button type="button" class="btn btn-success m-4">Success</button>
-          <button type="button" class="btn btn-danger m-4">Danger</button>
+          <button type="button" class="btn btn-primary m-4">
+            <div class="corner left_top"></div>
+            <div class="corner left_bottom"></div>
+            <div class="corner right_top"></div>
+            <div class="corner right_bottom"></div>
+            Primary
+          </button>
+          <button type="button" class="btn btn-secondary m-4">
+            <div class="corner left_top"></div>
+            <div class="corner left_bottom"></div>
+            <div class="corner right_top"></div>
+            <div class="corner right_bottom"></div>
+            Secondary
+          </button>
+          <button type="button" class="btn btn-success m-4">
+            <div class="corner left_top"></div>
+            <div class="corner left_bottom"></div>
+            <div class="corner right_top"></div>
+            <div class="corner right_bottom"></div>
+            Success
+          </button>
+          <button type="button" class="btn btn-danger m-4">
+            <div class="corner left_top"></div>
+            <div class="corner left_bottom"></div>
+            <div class="corner right_top"></div>
+            <div class="corner right_bottom"></div>
+            Danger
+          </button>
         </div>
       </div>
     </div>
